bafe111ab02af54fd0ce8f22d7ba66a7b8d6151f
Petr Dlouhý <petr.dlouhy@email.cz>
2017-06-06 16:23:02 +0200

    download data only for current year

--- stahni.sh
+++ stahni.sh
@@ -83,7 +83,9 @@ for i in $mista; do
           date_to="$y-12-31"
        fi
 
-       wget "https://unicam.camea.cz/Discoverer/StatsReports/bike-counter/stats-customer-hour-content/df/$date_from%2000:00:00/dt/$date_to%2000:00:00/sn/$i/format/csv" -O "data/${nazev[$i]}_$y.csv"
+       if [ $y = $year_to ]; then
+          wget "https://unicam.camea.cz/Discoverer/StatsReports/bike-counter/stats-customer-hour-content/df/$date_from%2000:00:00/dt/$date_to%2000:00:00/sn/$i/format/csv" -O "data/${nazev[$i]}_$y.csv"
+       fi
     done
     sed -i -e '$a\' data/${nazev[$i]}_*.csv
     cat data/${nazev[$i]}_*.csv | grep "^[^Č].*" > data-combined/${nazev[$i]}.csv
